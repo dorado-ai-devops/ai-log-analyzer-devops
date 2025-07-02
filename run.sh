9a0f0ff06a308db58aed4687616a66babe740614
root <root@deletepc.localdomain>
2025-07-02 14:35:47 +0200

    [Dorado-AI-DevOps] Uploaded Changes

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# RUN AI Log Analyzer
+#RUN AI Log Analyzer
 
 export PYTHONPATH=$(pwd)
 
 source ../venv/bin/activate
 
-python3 cli/generate.py --mode openai --logfile logs/example_jenkins.log
+python3 /root/devops-ai/ai-log-analyzer-devops/cli/generate.py --mode openai --logfile logs/example_jenkins.log
