23b12095a6b700833986bed23cf385191e94c451
root <root@deletepc.localdomain>
2025-07-02 21:45:42 +0200

    [Dorado-AI-DevOps] Uploaded Changes

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 IMAGE_NAME=log-analyzer
-VERSION=v0.1.6
+VERSION=v0.1.7
 CLUSTER_NAME=devops-ai
 REGISTRY=localhost:5000
 HELM_VALUES=../devops-ai-lab/manifests/helm-log-analyzer/values.yaml
--- lib/ollama_client.py
+++ lib/ollama_client.py
@@ -1,16 +1,18 @@
 # lib/ollama_client.py
 import requests
 import os
+
 class OllamaClient:
     def __init__(self, model="mistral"):
         self.model = model
         self.base_url = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
 
     def generate_completion(self, prompt):
-        response = requests.post(f"{self.base_url}/api/generate", json={
+        response = requests.post(f"{self.base_url}/api/chat", json={
             "model": self.model,
-            "prompt": prompt,
-            "stream": False
+            "messages": [
+                {"role": "user", "content": prompt}
+            ]
         })
         response.raise_for_status()
-        return response.json()["response"]
+        return response.json()["message"]["content"]
